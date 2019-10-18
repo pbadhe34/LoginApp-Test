f2c63659d759ecc3e33a04708b3ad7f62da71c76
Prakash Badhe <prakash.badhe@agilesoft.in>
2019-10-18 14:43:12 +0530

    The test script step defn updated.

--- src/stepDefinition/Cucumber_Selenium_Test_Steps.java
+++ src/stepDefinition/Cucumber_Selenium_Test_Steps.java
@@ -44,7 +44,7 @@ public class Cucumber_Selenium_Test_Steps {
 	@Given("^User is on Home Page$")
 	public void user_is_on_Home_Page() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions		 
-		 
+		 System.out.println("Cucumber starting with Selenium");
         driver.get("http://localhost:8080/LoginApp/");
 	    
 	}
